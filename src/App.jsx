d490e31e7cdf0d57724ba736fb1410214f11b071
Samuel Aliyari <samuelaliyari@outlook.de>
2023-12-12 19:11:52 +0100

    Gallery

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,38 @@ import { Data } from "./data/Data";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Detail from "./pages/Detail";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
 
 function App() {
+	// import all data and log in the Console
 	const allCocktails = useContext(Data);
-	console.log(allCocktails);
 
+	// Setter and getter for Data
+	const [data, setData] = useState(allCocktails);
+	console.log(data);
 	return (
 		<>
-			<Routes>
-				<Route
-					path='/'
-					element={<Home />}
-				/>
-				<Route
-					path='/detail/:id'
-					element={<Detail />}
-				/>
-			</Routes>
+			<Data.Provider value={{ data, setData }}>
+				<Header />
+				<Routes>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+					<Route
+						path='/detail/:id'
+						element={<Detail />}
+					/>
+
+					<Route
+						path='/gallery/:category'
+						element={<Gallery />}
+					/>
+				</Routes>
+				<Footer />
+			</Data.Provider>
 		</>
 	);
 }
--- src/components/GalleryItem.jsx
+++ src/components/GalleryItem.jsx
@@ -1,7 +1,26 @@
-const GalleryItem = () => {
+import { useContext, useEffect, useState } from "react";
+import { Data } from "../data/Data";
+
+const GalleryItem = ({ id, name, image }) => {
+	const { data, setdata } = useContext(Data);
+
+	const [itemData, setItemData] = useState([]);
+	const showItemData = () => {
+		setItemData(data.filter((cocktail) => cocktail.idDrink === id));
+	};
+	useEffect(() => {
+		if (itemData.length !== 0) {
+			console.log(itemData);
+		}
+	}, [itemData]);
+
 	return (
-		<article>
-			<h2>item</h2>
+		<article onClick={showItemData}>
+			<h2>{name}</h2>
+			<img
+				src={image}
+				alt={`Photo of ${name}`}
+			/>
 		</article>
 	);
 };
