08580eb0c45231c0784da59742b499c022611cb1
huangrunhong <huang.run.hong.1986@gmail.com>
2023-12-12 20:01:35 +0100

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import { useContext, useEffect, useState } from "react";
-import "./App.css";
+import "./App.scss";
 import { Data } from "./data/Data";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Detail from "./pages/Detail";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-	const allCocktails = useContext(Data);
-	console.log(allCocktails);
+  const allCocktails = useContext(Data);
+  console.log(allCocktails);
+  const [darkMode, setDarkMode] = useState(true);
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+  };
 
-	return (
-		<>
-			<Routes>
-				<Route
-					path='/'
-					element={<Home />}
-				/>
-				<Route
-					path='/detail/:id'
-					element={<Detail />}
-				/>
-			</Routes>
-		</>
-	);
+  return (
+    <section className={darkMode ? "darkMode" : "lightMode"}>
+      <Header onClick={changeMode} darkMode={darkMode} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+      <Footer />
+    </section>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,23 @@
-const Header = () => {
-	return (
-		<header>
-			<nav>
-				<h1>Logo</h1>
-			</nav>
-		</header>
-	);
+import "./Header.scss";
+import moon from "../images/moon.png";
+import sun from "../images/sun.png";
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <nav>
+        <h2>Drinks&Chill</h2>
+        <div>
+          <p>MENU</p>
+          <img onClick={props.onClick} src={props.darkMode ? sun : moon} />
+        </div>
+      </nav>
+      <article>
+        <h1>Cocktails & Getränke!</h1>
+        <p>HERZLICH WILLKOMMEN IN DER WELT DER COCKTAILS UND GETRÄNKE!</p>
+      </article>
+    </header>
+  );
 };
 
 export default Header;
