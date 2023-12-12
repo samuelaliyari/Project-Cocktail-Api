7331c93c09f2f17205ef8ab939961a2588109e63
Samuel Aliyari <samuelaliyari@outlook.de>
2023-12-12 16:38:22 +0100

    Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import { useContext, useEffect, useState } from "react";
 import "./App.css";
+import { Data } from "./data/Data";
 
 function App() {
+	const allCocktails = useContext(Data);
+	console.log(allCocktails);
+
 	return <></>;
 }
 
