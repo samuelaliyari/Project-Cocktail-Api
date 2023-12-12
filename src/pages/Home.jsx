1249e7a2f80b626fa5f52dbd80177e6fc963604b
Samuel Aliyari <samuelaliyari@outlook.de>
2023-12-12 23:38:23 +0100

    firstDay Complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,28 @@
 import { Link, useNavigate } from "react-router-dom";
-import { Data } from "../data/Data";
 import { useContext, useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
+import { Data } from "../data/Data";
 
 const Home = () => {
-	const navigate = useNavigate()
+	const navigate = useNavigate();
 	const { data, setData } = useContext(Data);
-	
-	const cocktailFilter = (searchInput) =>{
-		const filtered = data.filter((cocktail)=>{
-			return cocktail.strDrink.toLowerCase().includes(searchInput.toLowerCase())
-		})
-		
-		navigate(
-			'/gallery/searchresult',
-			{state: filtered}
-		)
-	}
+	console.log(data);
+	const cocktailFilter = (searchInput) => {
+		const filtered = data.filter((cocktail) => {
+			return cocktail.strDrink
+				.toLowerCase()
+				.includes(searchInput.toLowerCase());
+		});
+
+		navigate("/gallery/searchresult", { state: filtered });
+	};
 	return (
 		<main>
 			<h1>Home</h1>
-			<SearchBar searchFunc={cocktailFilter} btnShow={true}/>
+			<SearchBar
+				searchFunc={cocktailFilter}
+				btnShow={true}
+			/>
 			<Link to={`/gallery/alldata`}>all</Link>
 			<Link to={`/gallery/gin`}>Gin</Link>
 			<Link to={`/gallery/vodka`}>Vodka</Link>
