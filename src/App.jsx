f6bb9ec84cc3ea8d59f34c8f8a01f8f65d899272
johann3r <johannerrombach@gmail.comm>
2023-12-13 09:08:31 +0100

    Detail, Detailpopover und Detail SCSS hinzugefügt

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,23 @@ import { Data } from "./data/Data";
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Detail from "./pages/Detail";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-	const allCocktails = useContext(Data);
-	console.log(allCocktails);
+  const allCocktails = useContext(Data);
+  console.log(allCocktails);
 
-	return (
-		<>
-			<Routes>
-				<Route
-					path='/'
-					element={<Home />}
-				/>
-				<Route
-					path='/detail/:id'
-					element={<Detail />}
-				/>
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
