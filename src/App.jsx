6a1e02876f12b2f6c94f7291d3a656e8a36648e6
huangrunhong <huang.run.hong.1986@gmail.com>
2023-12-13 16:57:29 +0100

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import "./App.scss";
 import { Data } from "./data/Data";
 import Home from "./pages/Home";
@@ -21,17 +21,18 @@ function App() {
   console.log(data);
 
   return (
-    <section className={darkMode ? "darkMode" : "lightMode"}>
-      <Data.Provider value={{ data, setData }}>
+    <Data.Provider value={{ data, setData }}>
+      <section className={darkMode ? "darkMode" : "lightMode"}>
         <Header onClick={changeMode} darkMode={darkMode} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<Detail />} />
           <Route path="/gallery/:category" element={<Gallery />} />
+          <Route path="/newdrink" element={<NewDrink />} />
         </Routes>
         <Footer />
-      </Data.Provider>
-    </section>
+      </section>
+    </Data.Provider>
   );
 }
 
